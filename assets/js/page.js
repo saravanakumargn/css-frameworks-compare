92ddb26bbdf9b0fe195f1d17650a47285d90a8fd
Saravanakumar <saravanakumargn@gmail.com>
2016-12-16 22:06:31 +0530

    home page filter working copy

--- assets/js/page.js
+++ assets/js/page.js
@@ -3,63 +3,40 @@ angular.module('cssApp', [])
         $interpolateProvider.startSymbol('{#');
         $interpolateProvider.endSymbol('#}');
     })
+.filter('bytes', function() {
+	return function(bytes, precision) {
+		if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+		if (typeof precision === 'undefined') precision = 1;
+		var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+			number = Math.floor(Math.log(bytes) / Math.log(1024));
+		return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+	}
+})
     .controller('cssPageController', function ($http) {
         var cssScope = this;
-        cssScope.page = "test";
-        var pageUrl = $(location).attr('href');
-        var a = pageUrl.split('/');
-        var segment = a[a.length - 2];
-        var latestUrl = "",
-            repUrl = "";
-        var pageData;
-        cssScope.repoData = null;
-        cssScope.latestData = null;
+        cssScope.sortType = 'label';
+        cssScope.reverse = false;
+        cssScope.listData = [];
+
+
+        cssScope.sortBy = function (sortType) {
+//            console.log('sortBy');
+            cssScope.reverse = (cssScope.sortType === sortType) ? !cssScope.reverse : false;
+            cssScope.sortType = sortType;
+        };
+
 
         function getDataJSON() {
-            $http.get('../assets/data.json').then(successData, errorData);
+            $http.get('assets/data.json').then(successData, errorData);
         }
 
         function successData(result) {
-            angular.forEach(result.data, function (value, key) {
-                if (segment == key) {
-                    latestUrl = "https://api.github.com/repos/";
-                    repUrl = "https://api.github.com/repos/";
-
-                    latestUrl += value + "/releases/latest";
-                    repUrl += value;
-                    getPageData();
-                }
-            });
+            console.log(result.data);
+            cssScope.listData = result.data;
         }
 
         function errorData(error) {
             console.log(error);
         }
-
-        function getPageData() {
-            $http.get(latestUrl).then(successLatest, errorLatest);
-            $http.get(repUrl).then(successRep, errorRep);
-        }
-
-        function successLatest(result) {
-            cssScope.latestData = result.data;
-        }
-
-        function errorLatest(error) {
-
-        }
-
-        function successRep(result) {
-            cssScope.repoData = result.data;
-            //            console.log($scope.repoData);
-        }
-
-        function errorRep(error) {
-
-        }
-
-
         getDataJSON();
-
-
     });
